0749641240b1cd719e8a5830e8d537d090d9494f
sravan123-cod <mohannag@ gmail.com>
2025-01-24 17:48:38 +0530

    oparator

---
+++ oparator.py
@@ -0,0 +1,23 @@
+print("My name is Sravan")
+print("I am studying in 5th grade")
+print("I am studying in Ahp's school")
+print("My hobbies are drawing,painting,art,dance")
+print("My bestfriend name is Advaith")
+a =123456789
+b =987654321
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+a = False
+
+print("Data type of a is",type(a))
+b = 3.5
+
+print("Data type of b is",type(b))
+c = 453
+
+print("Data type of c is",type(c))
+d = "sravan"
+
+print("Data type of d is",type(d))
