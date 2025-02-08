a1d875c63d0df273c164935566db3e7328ec382a
sravan123-cod <mohannag@ gmail.com>
2025-02-08 15:40:18 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,17 @@
+num=17
+for i in range(1,11):
+    a=num*i
+    print("17 * %d =%d "% (i,a))
+
+num=23
+for j in range(1,11):
+    b=num*j
+    print("23* %d =%d"% (j,b))
+
+num=1
+sum=0
+while(num<=10):
+    sum=sum+num
+    num=num+1
+print("Sum of first 10 Natural numbers:", sum)
+        
