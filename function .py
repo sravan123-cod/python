8a6d92af5959d9d5692d10efe3cddb4de29e5c83
sravan123-cod <mohannag@ gmail.com>
2025-02-13 17:39:44 +0530

    function

---
+++ function .py
@@ -0,0 +1,17 @@
+def introduction(a):
+    print("My name is",a)
+name=input ("Enter your name")
+introduction (name)
+
+def introduction(b):
+    print("Hello Good morning",b)
+name=input ("Enter your name")
+introduction (name)
+
+def absolute(num):
+    if num>=0:
+        return num
+    else:
+        return -num
+print(absolute(-45))
+print(absolute(-54))
