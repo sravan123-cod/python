ed08f402d3048fd564bbf127e3e6c85fba709bbc
sravan123-cod <mohannag@ gmail.com>
2025-02-14 17:45:08 +0530

    oop

---
+++ orientated.py
@@ -0,0 +1,16 @@
+class fruit:
+    print("My favorate fruits are Mango,Grapes,Kiwi.")
+obj=fruit()
+# Write a program to create a class Parrot and perform the following tasks - Create a class variable species,
+#  Create a constructor that has instance variables - name and age, Create instances of class Parrot,
+#   passing arguments as well, Print Class variable by accessing it, Print Instance variables as well.
+class parrot:
+    species="bird"
+    def __init__(self, name,age):
+        self.name=name
+        self.age=age
+    def display(self):
+        print(self.name)
+        print(self.age)
+oge=parrot("sravan",10)
+oge.display()
