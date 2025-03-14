4c1aba40afc185680e43ae140401c2d8c716c03e
sravan123-cod <mohannag@ gmail.com>
2025-03-14 17:25:39 +0530

    phone

--- phonebook.py
+++ phonebook.py
@@ -10,6 +10,25 @@ def view_contacts():
             print(f"{name}:{details}")
 def search_contact(name):
     if name in address_book:
-        print(f"{name}:{address-book[name]}")
+        print(f"{name}:{address_book[name]}")
     else:
         print("contact not found.")
+
+while True:
+    print("/n1. add contact /n2. view contacts /n3.search contacts /n4.exit")
+    choice=input("enter choice:")
+    if choice=="1":
+        name=input("enter name:")
+        phone=input("enter phone:")
+        email=input("enter email:")
+        add_contact(name,phone ,email)
+    elif choice=="2":
+        view_contacts()
+    elif choice=="3":
+        name=input("enter your name yo search:")
+        search_contact(name)
+    elif choice=="4":
+        print("exitung address book.")
+        break
+    else:
+        print("invalid choice,try again.")
